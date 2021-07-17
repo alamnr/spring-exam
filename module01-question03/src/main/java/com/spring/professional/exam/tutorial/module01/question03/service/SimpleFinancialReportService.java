e0062fd358d0496088cd4fed33bfc99a8269d6f3
alamnr <alamnr@gmail.com>
2021-07-17 15:20:26 +0600

    why  interface

--- module01-question03/src/main/java/com/spring/professional/exam/tutorial/module01/question03/service/SimpleFinancialReportService.java
+++ module01-question03/src/main/java/com/spring/professional/exam/tutorial/module01/question03/service/SimpleFinancialReportService.java
@@ -1,14 +1,18 @@
 package com.spring.professional.exam.tutorial.module01.question03.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import com.spring.professional.exam.tutorial.module01.question03.dao.FinancialDataDao;
 import com.spring.professional.exam.tutorial.module01.question03.ds.FinancialYearSummary;
 import com.spring.professional.exam.tutorial.module01.question03.writer.FinancialReportWriter;
 
+@Component("SimpleFinancialReportService")
 class SimpleFinancialReportService {
     private final FinancialDataDao financialDataDao;
     private final FinancialReportWriter financialReportWriter;
 
-    SimpleFinancialReportService(FinancialDataDao financialDataDao, FinancialReportWriter financialReportWriter) {
+    SimpleFinancialReportService(@Qualifier("database-dao")FinancialDataDao financialDataDao, @Qualifier("database-writer") FinancialReportWriter financialReportWriter) {
         this.financialDataDao = financialDataDao;
         this.financialReportWriter = financialReportWriter;
     }
